9902b4898eec8f3920fa36f77971c44fa2af6129
NastiaCu <nastiacu24@gmail.com>
2022-09-19 23:41:50 +0300

    I want to die

--- binary_tree.c
+++ binary_tree.c
@@ -116,11 +116,11 @@ struct node *search(struct node *node, int data){
         return node;
     }
     
-    if (node->data < data){
+    if (node->data > data){
         node = search(node->right, data);
     }
    
-    else if (node-> data > data){
+    else if (node-> data < data){
         node = search(node->left, data);
     }
     
@@ -155,21 +155,179 @@ void Preorder(struct node *node){
 }
 
 
+void store(struct node *node){
+    int arr[101];
+    int index = 0;
+    
+    if (node == NULL){
+        return;
+    }
+    else{
+        store(node->left);
+        arr[index] = node->data;
+        (index)++;
+        store(node->right);
+    }
+}
+
+
+int count(struct node *node){
+    
+    int n = 0;
+    
+    if (node == NULL){
+        return 0;
+    }
+    
+    else {
+        n = count(node->left) + count(node->right) + 1;
+        return n;
+    }
+}
+
+
+void arrToBT(struct node *node, int arr[], int *index){
+    
+    if (node == NULL){
+        return;
+    }
+    
+    else {
+        arrToBT(node->left, arr, index);
+        node->data = arr[*index];
+        (*index)++;
+        arrToBT(node->right, arr, index);
+    }
+}
+
+
+//function to swap the position of two elements
+void swap(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+//function to find the largest element among the nodes
+
+void heapify(int arr[], int n, int i){
+    
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 1;
+    
+    if (left < n && arr[left] > arr[largest]){
+        largest = left;
+    }
+    
+    else if (right < n && arr[right] > arr[largest]){
+        largest = right;
+    }
+    
+    if (largest != i){
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n , largest);
+    }
+}
+
+
+void HeapSort(int arr[], int n){
+//    build max heap
+    int i;
+    for (i = n/2; i >= 0; i--){
+        heapify(arr, n , i);
+    }
+    
+//    heap sort
+    for (i = n-1; i >= 0; i--){
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+
+void BTtoBST(struct node *node){
+    
+    if (node == NULL){
+        return;
+    }
+    
+    else {
+        int n = count(node);
+        int *arr = NULL;
+        int i = 0;
+        store(node);
+        HeapSort(arr, n);
+        i = 0;
+        arrToBT(node, arr, &i);
+    }
+        
+}
+
+
+//write the binary tree to a file
+void serialize(struct node *node, char name[]){
+    FILE* file = fopen(name, "w");
+
+    if(file == NULL){
+        printf("Can't open the file\n");
+        exit(1);
+    }
+    
+    while(node != NULL)
+    {
+        fprintf(file, " %d ->", node->data);
+        if (node->right != NULL){
+            fprintf(file, " %d ->", node->right->data);
+        }
+        if (node->left != NULL){
+            fprintf(file, " %d ->", node->left->data);
+        }
+        node = node->left;
+    }
+    fprintf(file, " NULL");
+    fclose(file);
+}
+
+
+//read the binary tree from a file
+void deserialize(struct node **node, char name[]){
+    FILE* file = fopen(name, "r");
+
+    if(file == NULL){
+        printf("Can't read the file\n");
+        exit(2);
+    }
+    
+    int val = 0;
+    while(fscanf(file, " %d ->", &val) > 0)
+    {
+        insert(*node, val);
+    }
+    fclose(file);
+}
+
+
 int main(){
     
     struct node *root = NULL;
-    int option = 0, val;
+    int option = 0, val = 0;
     
     while(1){
         
         printf("Select the option:\n");
         printf("1 - insert new node\n");
         printf("2 - delete an element\n");
-        printf("3 - inorder traversal\n");
-        printf("4 - postorder traversal\n");
-        printf("5 - preorder traversal\n");
-        printf("6 - search a node\n");
-        printf("7 - end of execution\n");
+        printf("3 - search a node\n");
+        printf("4 - inorder traversal\n");
+        printf("5 - postorder traversal\n");
+        printf("6 - preorder traversal\n");
+        printf("7 - sort, transform in BST\n");
+        printf("8 - balance, transform in a balanced BT\n");
+        printf("9 - load the data in a new file\n");
+        printf("10 - read from a file\n");
+        printf("11 - end of execution\n");
         scanf("%d", &option);
     
     switch(option){
@@ -199,21 +357,6 @@ int main(){
         }
             
         case 3:{
-            Inorder(root);
-            break;
-        }
-            
-        case 4:{
-            Postorder(root);
-            break;
-        }
-            
-        case 5:{
-            Preorder(root);
-            break;
-        }
-        
-        case 6:{
             printf("Which node do you want to find? : ");
             scanf("%d", &val);
             
@@ -227,9 +370,48 @@ int main(){
             
             break;
         }
-        
+            
+        case 4:{
+            Inorder(root);
+            break;
+        }
+            
+        case 5:{
+            Postorder(root);
+            break;
+        }
+            
+        case 6:{
+            Preorder(root);
+            break;
+        }
+            
         case 7:{
+            BTtoBST(root);
+            Inorder(root);
+            break;
+        }
+        
+        case 8:{
+            break;
+        }
+            
+        case 9:{
+            serialize(root, "file.txt");
+            break;
+        }
+        
+        case 10:{
+            deserialize(&root, "file.txt");
+            break;
+        }
+            
+        case 11:{
             return 0;
+        }
+            
+        default:{
+            break;
         }
     }
     }
